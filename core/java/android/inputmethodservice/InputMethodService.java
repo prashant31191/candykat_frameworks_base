97efa572cb94605497318f8c7b2ccb6ee56596b4
errantzebra845 <errantzebra845@gmail.com>
2014-02-19 18:51:33 -0500

    "Update Halo/MultiView as well as add the option to Enable or Disable Halo from the Settings"
    Change-Id: I4b13bfcb3f2dc96295aefc39728d7ee144e7c27d

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -895,7 +895,8 @@ public class InputMethodService extends AbstractInputMethodService {
      * is currently running in fullscreen mode.
      */
     public void updateFullscreenMode() {
-        boolean isFullscreen = mShowInputRequested && onEvaluateFullscreenMode();
+        int mHaloEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.HALO_ENABLED, 0));
+        boolean isFullscreen = (mHaloEnabled != 1) ? (onEvaluateFullscreenMode() || onEvaluateSplitView()) : mShowInputRequested && onEvaluateFullscreenMode();
         boolean changed = mLastShowInputRequested != mShowInputRequested;
         if (mIsFullscreen != isFullscreen || !mFullscreenApplied) {
             changed = true;
